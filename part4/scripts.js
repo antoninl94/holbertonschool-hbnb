d0e8608da06ea79b1b7d86946369fb63a7b2a227
antoninl94 <9803@holbertonstudents.com>
2025-04-08 13:14:17 +0200

    aesthetic hotfix

--- part4/scripts.js
+++ part4/scripts.js
@@ -9,7 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
   if (loginForm) {
     loginForm.addEventListener('submit', async (event) => {
       event.preventDefault();
-      
+      const email = document.querySelector('#email').value;
+      const password = document.querySelector('#password').value;
     });
   }
 });
+async function loginUser(email, password) {
+  const response = await fetch('http://127.0.0.1:5000/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+  });
+}
+
+/*
+if (response.ok) {
+  const data = await response.json();
+  document.cookie = `token=${data.access_token}; path=/`;
+  window.location.href = 'index.html';
+} else {
+  alert('Login failed: ' + response.statusText);
+}
+*/
